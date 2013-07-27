ffeadb5225891de90d72d8ac91e738bc56eb1207
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-07-27 23:32:48 +0200

    Use codox for API generation.

--- src/leiningen/new/pirlib/project.clj
+++ src/leiningen/new/pirlib/project.clj
@@ -3,4 +3,6 @@
   :url "https://github.com/{{username}}/{{name}}"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :profiles {:dev {:plugins [[codox "0.6.4"]]
+                   :codox {:output-dir "codox"}}})
