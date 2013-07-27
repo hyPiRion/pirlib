ddeb8c8314598727a95610d7ad835a8a4546ddaf
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-07-27 17:51:30 +0200

    Read in opts and display proper error messages.

--- src/leiningen/new/pirlib.clj
+++ src/leiningen/new/pirlib.clj
@@ -4,13 +4,27 @@
 
 (def render (renderer "pirlib"))
 
+(defn parse-opts [opt-list]
+  (when (odd? (count opt-list))
+    (main/abort
+     "Error: pirlib template expects :key value pairs after the library name."))
+  (apply hash-map opt-list))
+
+(defn expect-keys [opts expected-keys]
+  (doseq [expected-key expected-keys]
+    (when-not (contains? opts (str ":" expected-key))
+      (main/abort (str "Error: No " expected-key " specified.")))))
+
 (defn pirlib
   "FIXME: write documentation"
-  [name]
-  (let [data {:name name
+  [name & opt-list]
+  (let [{username ":username" :as opts} (parse-opts opt-list)
+        data {:name name
               :sanitized (name-to-path name)
+              :username username
               :year (year)}
         render #(render % data)]
+    (expect-keys opts ["username"])
     (main/info (str "Generating a pirlib named " name "..."))
     (->files data
              ["LICENSE" (render "LICENSE")]
