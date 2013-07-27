36f0d156014992a1609ac554bfe4ddbbd4c581dc
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-07-28 01:17:43 +0200

    pirlib 0.1.0
    Signed-off-by: Jean Niklas L'orange <jeannikl@hypirion.com>

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pirlib/lein-template "0.1.0-SNAPSHOT"
+(defproject pirlib/lein-template "0.1.0"
   :description "Default library template with hyPiRion's preferred settings."
   :url "https://github.com/hyPiRion/pirlib"
   :license {:name "Eclipse Public License"
