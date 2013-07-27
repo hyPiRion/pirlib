10e66e7fa789010bbc4d165013d3b8d92817e70a
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-07-28 01:15:49 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
 (defproject pirlib/lein-template "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "Default library template with hyPiRion's preferred settings."
+  :url "https://github.com/hyPiRion/pirlib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :deploy-branches ["stable"]
   :eval-in-leiningen true)
