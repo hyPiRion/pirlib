c3d035a404b6494a4adfa2777a91ce9f2ad034d1
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-07-28 00:54:12 +0200

    Only deploy in stable branch.

--- src/leiningen/new/pirlib/project.clj
+++ src/leiningen/new/pirlib/project.clj
@@ -5,4 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]]
   :profiles {:dev {:plugins [[codox "0.6.4"]]
-                   :codox {:output-dir "codox"}}})
+                   :codox {:output-dir "codox"}}}
+  :deploy-branches ["stable"])
