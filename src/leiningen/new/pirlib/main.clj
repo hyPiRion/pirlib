a11b9da309b9c17d4228c79f352eb34210620875
Jean Niklas L'orange <jeannikl@hypirion.com>
2013-07-27 19:56:33 +0200

    Add in empty template main and test files.

---
+++ src/leiningen/new/pirlib/main.clj
@@ -0,0 +1,3 @@
+(ns {{namespace}}.main)
+
+;; Place your "main" code here, and consider to rename the namespace too.
---
+++ src/leiningen/new/pirlib/test.clj
@@ -0,0 +1,5 @@
+(ns {{namespace}}.test.main
+  (:require [clojure.test :refer :all]
+            [{{namespace}}.main :refer :all]))
+
+;; If the main class is renamed, rename this file as well.
